545050be005093f2d9a8e2fed3c702972e37142e
Nasir <nasirbhatti.nk@gmail.com>
2024-01-15 19:54:44 +0500

    Added a new book functionality and rendered in the browser console

--- script.js
+++ script.js
@@ -14,5 +14,22 @@ const hideAddBookForm = () => {
     addbook.style.display = 'block';
 }
 
+const addNewBook = () => {
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const isRead = document.querySelector("#isRead").checked;
+
+    console.log('Book Data:', {title, author, pages, isRead})
+
+    hideAddBookForm();
+
+}
+
 addbook.addEventListener('click', showAddBookForm);
 close.addEventListener('click', hideAddBookForm);
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addNewBook();
+});
