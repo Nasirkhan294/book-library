1245dafe4dd877fde6f7bcf9f597014f0f9ff6ef
Nasir <nasirbhatti.nk@gmail.com>
2024-01-16 17:08:57 +0500

    Add new book by submitting the form

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-const addbook = document.querySelector('.add-btn');
-const close = document.querySelector('.fa-times');
-const form = document.querySelector('form');
-
+const addbook = document.querySelector(".add-btn");
+const close = document.querySelector(".fa-times");
+const form = document.querySelector("form");
 
 const showAddBookForm = () => {
-    form.reset();
-    form.style.display = 'flex'
-    addbook.style.display = 'none';
-}
+  form.reset();
+  form.style.display = "flex";
+  addbook.style.display = "none";
+};
 
 const hideAddBookForm = () => {
-    form.style.display = 'none'
-    addbook.style.display = 'block';
-}
+  form.style.display = "none";
+  addbook.style.display = "block";
+};
 
 const addNewBook = () => {
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const pages = document.querySelector("#pages").value;
-    const isRead = document.querySelector("#isRead").checked;
+  const inputTitle = document.querySelector("#title").value;
+  const inputAuthor = document.querySelector("#author").value;
+  const inputPages = document.querySelector("#pages").value;
+  const inputIsRead = document.querySelector("#isRead").checked;
+  const book = document.querySelector(".book");
 
-    console.log('Book Data:', {title, author, pages, isRead})
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const isRead = document.createElement('button');
 
-    hideAddBookForm();
+  title.textContent = inputTitle;
+  author.textContent = inputAuthor;
+  pages.textContent = inputPages;
+  isRead.textContent = inputIsRead;
 
-}
+  bookData = document.createElement("li");
 
-addbook.addEventListener('click', showAddBookForm);
-close.addEventListener('click', hideAddBookForm);
+  bookData.appendChild(title);
+  bookData.appendChild(author);
+  bookData.appendChild(pages);
+  bookData.appendChild(isRead);
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    addNewBook();
+  book.appendChild(bookData);
+
+  hideAddBookForm();
+};
+
+addbook.addEventListener("click", showAddBookForm);
+close.addEventListener("click", hideAddBookForm);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  addNewBook();
 });
